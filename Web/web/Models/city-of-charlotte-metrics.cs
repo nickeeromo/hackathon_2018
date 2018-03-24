8a3310938d32e89a1d6922d2b7fb0b224a09ea79
Nick Edwards <nickeeromo@aol.com>
2018-03-24 04:01:04 -0400

    Added one more

--- Web/web/Models/city-of-charlotte-metrics.cs
+++ Web/web/Models/city-of-charlotte-metrics.cs
@@ -7,5 +7,17 @@ namespace WebApplication1.Models
 {
     public class city_of_charlotte_metrics
     {
+        string Parcel;
+        string _2017Address;
+        string CITY;
+        const string STATE = "NC";
+        string ZIP;
+        string _2017TotalSubUnits;
+        string _2017Source; // Only a few choices.
+        string _2017PropertyName;
+        string NPA;
+        int HousingUnits;
+        double LAT;
+        double LONG;
     }
 }
