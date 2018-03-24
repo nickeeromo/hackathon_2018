53ce257b5d5655ba8b3578315e26e68482ef343d
skipbassey <skipbassey@gmail.com>
2018-03-24 00:15:29 -0400

    commit

--- src/main/java/com/example/demo/HackathonApplication.java
+++ src/main/java/com/example/demo/HackathonApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HackathonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HackathonApplication.class, args); //
+		SpringApplication.run(HackathonApplication.class, args); 
 	}
 }
