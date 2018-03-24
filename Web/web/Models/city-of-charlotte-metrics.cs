29e055e43192d285b600dc6dd00425659da1d785
Nick Edwards <nickeeromo@aol.com>
2018-03-24 03:54:02 -0400

    Added models to Web.

---
+++ Web/web/Models/city-of-charlotte-metrics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class city_of_charlotte_metrics
+    {
+    }
+}
